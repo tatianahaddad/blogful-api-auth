1c537f50e0aea4abcee068b49894bb215bb2de7c
Tatiana Haddad <tatianadhaddad@gmail.com>
2019-08-05 22:23:00 -0700

    initial commit

---
+++ test/setup.js
@@ -0,0 +1,14 @@
+process.env.TZ = 'UCT'
+process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'test-jwt-secret'
+
+require('dotenv').config()
+
+process.env.TEST_DB_URL = process.env.TEST_DB_URL
+  || "postgresql://dunder-mifflin@localhost/blogful-auth-test"
+
+const { expect } = require('chai')
+const supertest = require('supertest')
+
+global.expect = expect
+global.supertest = supertest
